212fd6f6e66845d4470dde37246ebe2dc99dc1aa
linguangxiong <854501715@qq.com>
2019-07-17 14:37:28 +0800

    feat:complete add and save company to h2

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -1,19 +1,41 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.dao.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyResource {
+
+
+    @Autowired
+    CompanyRepository companyRepository;
+
     
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-        return null;
+
+        return companyRepository.findAll();
+
     }
     
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
-        return null;
+
+
+        return companyRepository.save(company);
     }
+
+
+    @PutMapping
+    public Company update(@RequestBody Company company){
+
+        return companyRepository.save(company);
+
+    }
+
+
+
 }
---
+++ src/main/java/com/tw/apistackbase/dao/CompanyRepository.java
@@ -0,0 +1,18 @@
+package com.tw.apistackbase.dao;
+
+import com.tw.apistackbase.core.Company;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created with IDEA
+ *
+ * @author:linGuangXiong
+ * @Date:2019/7/17
+ * @Time:14:12
+ * @description:
+ */
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+
+
+
+}
